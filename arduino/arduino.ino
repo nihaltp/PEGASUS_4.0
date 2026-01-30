9e889dc74a54f7fff96aa4f21773f9be5b19e403
NIHAL  T P <nihaltpnki@gmail.com>
2026-01-30 19:00:43 +0530

    Remove example movement and servo actions from loop function

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -8,15 +8,5 @@ void setup() {
 }
 
 void loop() {
-  moveTo(200, 0);      // X forward
-  delay(500);
-
-  moveTo(0, 200);      // Y forward
-  delay(500);
-
-  setServoAngle(45);   // example action
-  delay(1000);
-
-  setServoAngle(90);
-  delay(2000);
+  processSerial();
 }
